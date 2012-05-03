b42d9d0058cf1071775b6591698fd06bd1c486cc
luffycn <kingla_pei@163.com>
2012-05-03 13:27:43 +0800

    Initial

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Quora::Application.routes.draw do
+  
+  resources :notifications
+
+  # -- 用户登录认证相关 --
+  root :to=>"index#index"
+  get  '/login'  => 'sessions#new'
+  post '/login'  => 'sessions#create'
+  get  '/logout' => 'sessions#destroy'
+  
+  get  '/signup'        => 'signup#form'
+  post '/signup_submit' => 'signup#form_submit'
+end
