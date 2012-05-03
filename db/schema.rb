a53f89adc855ce0bdbc54f8125c06b5a82d0dd02
luffycn <kingla_pei@163.com>
2012-05-03 14:26:16 +0800

    Add audited gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120315071713) do
+ActiveRecord::Schema.define(:version => 20120503062549) do
+
+  create_table "activities", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "date"
+    t.integer  "creator_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "activity_assigns", :force => true do |t|
+    t.integer  "activity_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "answer_votes", :force => true do |t|
     t.integer  "user_id"
@@ -29,6 +45,39 @@ ActiveRecord::Schema.define(:version => 20120315071713) do
     t.datetime "updated_at"
   end
 
+  create_table "audits", :force => true do |t|
+    t.integer  "auditable_id"
+    t.string   "auditable_type"
+    t.integer  "associated_id"
+    t.string   "associated_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "username"
+    t.string   "action"
+    t.text     "audited_changes"
+    t.integer  "version",         :default => 0
+    t.string   "comment"
+    t.string   "remote_address"
+    t.datetime "created_at"
+  end
+
+  add_index "audits", ["associated_id", "associated_type"], :name => "associated_index"
+  add_index "audits", ["auditable_id", "auditable_type"], :name => "auditable_index"
+  add_index "audits", ["created_at"], :name => "index_audits_on_created_at"
+  add_index "audits", ["user_id", "user_type"], :name => "user_index"
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "categories", ["parent_id"], :name => "index_categories_on_parent_id"
+
   create_table "comments", :force => true do |t|
     t.integer  "model_id"
     t.string   "model_type"
@@ -39,6 +88,108 @@ ActiveRecord::Schema.define(:version => 20120315071713) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "comments", ["creator_id"], :name => "index_comments_on_creator_id"
+  add_index "comments", ["model_id", "model_type"], :name => "index_comments_on_model_id_and_model_type"
+  add_index "comments", ["reply_comment_id"], :name => "index_comments_on_reply_comment_id"
+  add_index "comments", ["reply_comment_user_id"], :name => "index_comments_on_reply_comment_user_id"
+
+  create_table "courses", :force => true do |t|
+    t.string   "name",       :default => "", :null => false
+    t.string   "cid"
+    t.string   "department"
+    t.string   "location"
+    t.integer  "teacher_id"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "courses", ["teacher_id"], :name => "index_courses_on_teacher_id"
+
+  create_table "homework_assigns", :force => true do |t|
+    t.integer  "student_id"
+    t.integer  "homework_id"
+    t.text     "content"
+    t.boolean  "is_submit",    :default => false
+    t.datetime "submitted_at"
+    t.boolean  "has_finished", :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "homework_assigns", ["homework_id"], :name => "index_homework_assigns_on_homework_id"
+  add_index "homework_assigns", ["student_id"], :name => "index_homework_assigns_on_student_id"
+
+  create_table "homework_student_upload_requirements", :force => true do |t|
+    t.integer  "creator_id"
+    t.integer  "homework_id"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "homework_student_upload_requirements", ["creator_id"], :name => "index_homework_student_upload_requirements_on_creator_id"
+  add_index "homework_student_upload_requirements", ["homework_id"], :name => "index_homework_student_upload_requirements_on_homework_id"
+
+  create_table "homework_student_uploads", :force => true do |t|
+    t.integer  "creator_id"
+    t.integer  "attachement_id"
+    t.string   "attachement_file_name"
+    t.string   "attachement_content_type"
+    t.integer  "attachement_file_size"
+    t.datetime "attachement_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "homework_student_uploads", ["creator_id"], :name => "index_homework_student_uploads_on_creator_id"
+
+  create_table "homework_teacher_attachements", :force => true do |t|
+    t.integer  "creator_id"
+    t.integer  "homework_id"
+    t.string   "attachement_file_name"
+    t.string   "attachement_content_type"
+    t.integer  "attachement_file_size"
+    t.datetime "attachement_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "homework_teacher_attachements", ["creator_id"], :name => "index_homework_teacher_attachements_on_creator_id"
+  add_index "homework_teacher_attachements", ["homework_id"], :name => "index_homework_teacher_attachements_on_homework_id"
+
+  create_table "homeworks", :force => true do |t|
+    t.integer  "creator_id"
+    t.string   "title"
+    t.text     "content"
+    t.integer  "course_id"
+    t.datetime "deadline"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "homeworks", ["course_id"], :name => "index_homeworks_on_course_id"
+  add_index "homeworks", ["creator_id"], :name => "index_homeworks_on_creator_id"
+
+  create_table "media_files", :force => true do |t|
+    t.string   "entry_file_name"
+    t.string   "entry_content_type"
+    t.integer  "entry_file_size"
+    t.datetime "entry_updated_at"
+    t.string   "place"
+    t.text     "desc"
+    t.integer  "creator_id"
+    t.integer  "category_id"
+    t.string   "video_encode_status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "file_merged"
+    t.string   "real_file_name"
+  end
+
+  add_index "media_files", ["category_id"], :name => "index_media_files_on_category_id"
+  add_index "media_files", ["creator_id"], :name => "index_media_files_on_creator_id"
 
   create_table "notifications", :force => true do |t|
     t.integer  "user_id"
@@ -88,6 +239,89 @@ ActiveRecord::Schema.define(:version => 20120315071713) do
     t.datetime "updated_at"
   end
 
+  create_table "statuses", :force => true do |t|
+    t.text     "content"
+    t.integer  "creator_id"
+    t.integer  "repost_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", :force => true do |t|
+    t.string   "real_name",  :default => "", :null => false
+    t.string   "sid"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "students", ["user_id"], :name => "index_students_on_user_id"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "creator_id"
+    t.integer  "model_id"
+    t.string   "model_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["creator_id"], :name => "index_taggings_on_creator_id"
+  add_index "taggings", ["model_id", "model_type"], :name => "index_taggings_on_model_id_and_model_type"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teachers", :force => true do |t|
+    t.string   "real_name",  :default => "", :null => false
+    t.string   "tid"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "teachers", ["user_id"], :name => "index_teachers_on_user_id"
+
+  create_table "team_status_links", :force => true do |t|
+    t.integer  "team_id"
+    t.integer  "status_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "team_students", :force => true do |t|
+    t.integer  "team_id"
+    t.integer  "student_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "team_students", ["student_id"], :name => "index_team_students_on_student_id"
+  add_index "team_students", ["team_id"], :name => "index_team_students_on_team_id"
+
+  create_table "teams", :force => true do |t|
+    t.string   "name",       :default => "", :null => false
+    t.string   "cid"
+    t.integer  "teacher_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "teams", ["teacher_id"], :name => "index_teams_on_teacher_id"
+
+  create_table "todos", :force => true do |t|
+    t.integer  "creator_id"
+    t.text     "content"
+    t.integer  "date"
+    t.boolean  "completed",  :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name",                      :default => "", :null => false
     t.string   "hashed_password",           :default => "", :null => false
@@ -109,11 +343,18 @@ ActiveRecord::Schema.define(:version => 20120315071713) do
     t.datetime "updated_at"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["name"], :name => "index_users_on_name"
+
   create_table "vote_items", :force => true do |t|
     t.integer  "vote_id"
     t.string   "item_title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "vote_result_items", :force => true do |t|
@@ -136,6 +377,7 @@ ActiveRecord::Schema.define(:version => 20120315071713) do
     t.integer  "select_limit"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "kind",         :default => "TEXT"
   end
 
 end
